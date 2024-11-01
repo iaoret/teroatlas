98e227b563514121c3fbbe3b7363ec902d6488c2
Victor Marotta <victordossantosmarotta@gmail.com>
2024-11-01 17:01:27 -0300

    fix: fixed dashboard thumbnails

--- src/components/dashboard-item.tsx
+++ src/components/dashboard-item.tsx
@@ -15,13 +15,13 @@ export default function DashboardItem(props: {
   onClick?: () => void;
 }) {
   return (
-    <div>
+    <div className="flex flex-row items-center justify-center">
       <Card
-        className="bg-primary-foreground hover:bg-zinc-800 hover:shadow-lg transition-all duration-200 rounded-lg hover:cursor-pointer max-w-[400px]"
+        className="bg-primary-foreground hover:bg-zinc-800 hover:shadow-lg transition-all duration-200 rounded-lg hover:cursor-pointer max-w-[400px] w-full"
         onClick={props.onClick}
       >
         <div className="h-full flex flex-row items-center">
-          <div className="h-full w-30 rounded-full bg-black flex flex-col justify-center items-center">
+          <div className="w-full rounded-full bg-black flex flex-col justify-center items-center">
             {props.image}
           </div>
           <div></div>
